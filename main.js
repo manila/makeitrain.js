80167ebca6ad6de22edbe6dcd919da05b8abfc5c
manila <manila@users.noreply.github.com>
2019-01-24 23:19:48 -0700

    Added canvas rain

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 var canvas = document.getElementById("rainbg");
 var ctx = canvas.getContext("2d");
 
+pixels = [];
+
+const generateRainDrop = () => {
+	pixels.push({x: Math.floor(Math.random() * 500), y: 0});
+}
+
+const drawRain = () => {
+	for (let i = 0; i < pixels.length; i++)
+	{
+		ctx.fillStyle = "#F00";
+        	ctx.fillRect(pixels[i].x, pixels[i].y, 1, 1);
+	}
+}
+
+const makeItRain = () => {
+	for (let i = 0; i < pixels.length; i++)
+	{
+		pixels[i].y++;
+	}
+}
+
 function loop(timestamp) {
-	console.log(timestamp);
+	generateRainDrop();
+	drawRain();
+	makeItRain();
 	window.requestAnimationFrame(loop);
 }
 
